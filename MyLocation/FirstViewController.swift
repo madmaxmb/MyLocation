62d95a2a842ebddfea1a2ce222ce7a23d3f27bf8
Максим <maksim@mbp-maksim.local>
2016-03-01 08:46:14 +0300

    Initial Commit

---
+++ MyLocation/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MyLocation
+//
+//  Created by Максим on 01.03.16.
+//  Copyright © 2016 Maxim. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
